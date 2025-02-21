a91b72be37b28dc8894f7ab032555d707b0022c4
Mathuritika <mathurritikajpr@gmail.com>
2025-02-21 22:14:28 +0530

    Experiment 6 - Importing packages (Continuation of Account.java)

---
+++ Main.java
@@ -0,0 +1,10 @@
+import Balance.Account;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Account myAccount = new Account(3500000.00);
+        
+        myAccount.Display_Balance();
+    }
+}
