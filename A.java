f8bfa2024b7232d74b174f1f945e0cb5c5591ed3
Mathuritika <mathurritikajpr@gmail.com>
2025-03-02 18:53:10 +0530

    Experiment 6 - Q2. - Class A inside package P

---
+++ A.java
@@ -0,0 +1,19 @@
+package P; 
+
+public class A {
+    public void publicMethod() {
+        System.out.println("Public method");
+    }
+    
+    protected void protectedMethod() {
+        System.out.println("Protected method");
+    }
+    
+    void defaultMethod() {
+        System.out.println("Default method");
+    }
+    
+    private void privateMethod() {
+        System.out.println("Private method");
+    }
+}
